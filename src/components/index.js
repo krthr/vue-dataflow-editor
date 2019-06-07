519d569e68a4c6519df51ca5cc9d3cb8757b0183
krthr <twilson@uninorte.edu.co>
2019-06-07 09:55:34 -0500

    typo error

--- src/components/index.js
+++ src/components/index.js
@@ -12,7 +12,7 @@ const Components = {
 };
 
 Object.keys(Components).forEach(name => {
-  Vue.component(name, components[name]);
+  Vue.component(name, Components[name]);
 });
 
 export default Components;
